9f8ddf92f3010b53cd6fa4ae5002c594643b87a5
aleksandrasucho <aleksandrasucho@gmail.com>
2023-08-15 23:31:11 +0100

    Add logo and welcome to constants.py

---
+++ constants.py
@@ -0,0 +1,20 @@
+"""
+Stores all the things to be used in the game
+Game Logo, Welcome, Game Options Menu,
+Game Levels Options, Game Rules and Hangman Stages
+"""
+#Logo from  patorjk.com
+logo = [
+    """
+ __    __       ___      .__   __.   _______ .___  ___.      ___      .__   __. 
+|  |  |  |     /   \     |  \ |  |  /  _____||   \/   |     /   \     |  \ |  | 
+|  |__|  |    /  ^  \    |   \|  | |  |  __  |  \  /  |    /  ^  \    |   \|  | 
+|   __   |   /  /_\  \   |  . `  | |  | |_ | |  |\/|  |   /  /_\  \   |  . `  | 
+|  |  |  |  /  _____  \  |  |\   | |  |__| | |  |  |  |  /  _____  \  |  |\   | 
+|__|  |__| /__/     \__\ |__| \__|  \______| |__|  |__| /__/     \__\ |__| \__| 
+""",
+"Welcome to Hangman!",
+"May your guesses be as clever as a detective's hunch."
+]
+
+
