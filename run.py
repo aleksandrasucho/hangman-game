a4b5687aa5c2db6120be82688598521290f6b427
aleksandrasucho <aleksandrasucho@gmail.com>
2023-08-16 01:31:55 +0100

    Add game_welcome function to display logo

--- run.py
+++ run.py
@@ -8,3 +8,10 @@ from constants import logo
 from constants import game_over
 from constants import you_win
 
+def game_welcome ():
+    """
+    Game Logo from patorjk.com
+    Welcomes the user and asks for the name
+    """
+    for line in logo:
+        print(f"{Fore.GREEN+Style.BRIGHT}{logo}")
