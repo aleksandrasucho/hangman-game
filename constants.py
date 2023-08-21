3afb5a1de6c5674d6ce76135818ab68bf6921ed5
aleksandrasucho <aleksandrasucho@gmail.com>
2023-08-21 11:49:09 +0100

    Add game_variable file with game rules, menu and game_level functions in it

--- constants.py
+++ constants.py
@@ -99,27 +99,3 @@ ____    ____  ______    __    __     ____    __    ____  __  .__   __.  __
                                                                             
     """
 ]
-#Game Rules
-game_rules = [
-    "How to play Rules!",
-    "This is a word-guessing extravaganza!",
-    "Guess just 1 letter each time! No alphabet showers, please.",
-    "Oops! A wrong guess costs you a life :( Sorry, it's business, not personal.",
-    "Watch out! Your hangman's gonna start flexing its construction skills.",
-    "Keep it alive! But if your lives hit rock bottom (0), well, things get a bit... hanging.",
-    "No worries, a restart button's your safety net. Phew!",
-    "Play again, triumph, and sport that virtual Hangman champ crown! "
-]
-
-#Shows the menu where the player can see the rules and start the game
-menu = [
-    "Press 1 to Read The Hang-Hangman Rules",
-    "Press 2 to Select the difficulty to play the game"
-]
-
-#Gives the player an option to choose difficulty of the words
-game_level = [
-    "Select the level you wish to play at.",
-    "Press E for Easy words!",
-    "Press D for Difficult words!"
-]
